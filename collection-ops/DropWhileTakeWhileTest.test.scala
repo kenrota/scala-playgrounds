825577d93e77673ef90444d6881de6dda134811a
kenrota <16278068+kenrota@users.noreply.github.com>
2025-04-13 10:34:36 +0900

    Collection operations (#2)

---
+++ collection-ops/DropWhileTakeWhileTest.test.scala
@@ -0,0 +1,53 @@
+//> using scala 3.6.4
+//> using test.dep org.scalameta::munit::1.1.0
+
+class DropWhileTakeWhileTest extends munit.FunSuite {
+  def extractFirstIncreasingSegment(data: List[Int]): List[Int] = {
+    data
+      .dropWhileWindow((prev, curr) => curr <= prev)
+      .takeWhileWindow((prev, curr) => curr > prev)
+  }
+
+  extension (list: List[Int])
+    def dropWhileWindow(cond: (Int, Int) => Boolean): List[Int] = {
+      val remainFirst = list
+        .sliding(2)
+        .zipWithIndex
+        .dropWhile {
+          case (List(a, b), _) => cond(a, b)
+          case _               => false
+        }
+        .nextOption()
+
+      remainFirst match {
+        case Some((_, idx)) => list.drop(idx)
+        case None           => List.empty
+      }
+    }
+
+    def takeWhileWindow(cond: (Int, Int) => Boolean): List[Int] = {
+      val pairs = list.sliding(2).collect { case List(a, b) => (a, b) }.toList
+
+      val taken = pairs.takeWhile { case (a, b) => cond(a, b) }
+      if taken.isEmpty then List.empty
+      else taken.map(_._1) :+ taken.last._2
+    }
+
+  test("return the first increasing segment") {
+    val data = List(10, 8, 7, 8, 9, 13, 11, 15)
+    val expected = List(7, 8, 9, 13)
+    assertEquals(extractFirstIncreasingSegment(data), expected)
+  }
+
+  test("return an empty list when no increasing segment is found") {
+    val data = List(10, 8, 7, 6, 3)
+    val expected = List()
+    assertEquals(extractFirstIncreasingSegment(data), expected)
+  }
+
+  test("return the entire list when all elements are in increasing order") {
+    val data = List(7, 8, 9, 13, 14, 15)
+    val expected = List(7, 8, 9, 13, 14, 15)
+    assertEquals(extractFirstIncreasingSegment(data), expected)
+  }
+}
---
+++ collection-ops/WithDefaultTest.test.scala
@@ -0,0 +1,40 @@
+//> using scala 3.6.4
+//> using test.dep "org.scalameta::munit::1.1.0"
+
+class WithDefaultTest extends munit.FunSuite {
+  val userNames: Map[String, String] = Map(
+    "user1" -> "Alice",
+    "user2" -> "Bob"
+  ).withDefault(id => s"Guest-$id") // Provide a fallback name for unknown users
+
+  val logs: List[(String, String)] = List(
+    // userId, action
+    ("user1", "login"),
+    ("user2", "purchase"),
+    ("user42", "login"), // Non-existing user
+    ("user99", "logout") // Non-existing user
+  )
+
+  def enrichLogsWithUserNames(
+      logs: List[(String, String)]
+  ): List[(String, String)] = {
+    logs.map { case (userId, action) =>
+      val name = userNames(userId)
+      (name, action)
+    }
+  }
+
+  test("enrich logs with real and guest user names") {
+    val result = enrichLogsWithUserNames(logs)
+
+    assertEquals(
+      result,
+      List(
+        ("Alice", "login"),
+        ("Bob", "purchase"),
+        ("Guest-user42", "login"),
+        ("Guest-user99", "logout")
+      )
+    )
+  }
+}
